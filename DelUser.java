b73e7125a9703cff789174d2dd448f0bcae0b2d9
Andrey <Andrey759@list.ru>
2014-10-05 22:21:08 +0400

    Список удалившихся друзей работает

---
+++ DelUser.java
@@ -0,0 +1,20 @@
+import java.util.Calendar;
+
+public class DelUser extends User {
+
+    private Calendar date;
+
+    public DelUser(int id, String pic, String page, Gender gender, String name, Calendar date) {
+        super(id, pic, page, gender, name);
+        this.date = date;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " date: " + String.format("%tY-%<tm-%<td %<tH:%<tM:%<tS", date);
+    }
+}
